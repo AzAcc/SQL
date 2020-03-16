15d0653c3cdb43f3d9bdf6303b7ff44a1580b49c
AzAcc <zzaaxvat@gmail.com>
2020-03-16 16:27:57 +0300

    Delete temporary_table.sql

--- create_tables/temporary_table.sql
+++
@@ -1,5 +0,0 @@
---Создание таблицы коордитан аварий в штате 1--
-CREATE GLOBAL TEMPORARY 
-TABLE coor_state_1   
-ON COMMIT PRESERVE ROWS    
-AS SELECT ID,start_lat,end_lat,start_lng, end_lng FROM accidents WHERE state_id = 1;
