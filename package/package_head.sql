b0290060ddbf4155ae17359517addc9814a2f882
AzAcc <zzaaxvat@gmail.com>
2020-03-16 16:28:28 +0300

    Delete package_head.sql

--- package/package_head.sql
+++
@@ -1,8 +0,0 @@
-create or replace PACKAGE FILL_ACC IS
-    PROCEDURE data_ins;
-    PROCEDURE update_record;
-    PROCEDURE fill_city;
-    PROCEDURE fill_street;
-    PROCEDURE fill_source;
-    PROCEDURE fill_tmc;
-END fill_acc; 
